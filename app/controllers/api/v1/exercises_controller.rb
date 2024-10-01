ecf174cbdee77c9dd86143044715ed12e915a290
Aasmund Gravem <aasmund.gravem@gmail.com>
2024-10-01 18:22:10 +0100

    Added exercise model and controller with logic for indexing and new exercise.

---
+++ app/controllers/api/v1/exercises_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::ExercisesController < ApplicationController
+  def index
+    exercise = Exercise.all.order(created_at: :desc)
+    render json: exercise
+  end
+
+  def create
+    exercise = Exercise.create!(exercise_params)
+    if exercise
+      render json: exercise
+    else
+      render json: exercise.errors
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+  def exercise_params
+    params.permit(:name, :image, :trainings, :instruction)
+  end
+end
